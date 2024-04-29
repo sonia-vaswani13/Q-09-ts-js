aa78c5c3c8044d4480451b0edb1eb0f266ca9c04
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 10:28:55 -0700

    Q 09 ts/js node js of 45 assignments
    Q 09 ts/js node js of 45 assignments

---
+++ Q9.ts
@@ -0,0 +1,9 @@
+//Q9.Favorite Number: Store your favorite number in a variable.
+ //Then, using that variable, create a message that reveals your favorite number.
+ // Print that message.
+ let NUM: Number =6394433
+ console.log(`MY favourite number is ${NUM}`)
+
+
+
+
